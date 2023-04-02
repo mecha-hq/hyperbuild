55d5aea2da2de6ad2bcd416ad77380860f5ca40a
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-04-02 09:22:56 +0200

    feat(cli): introduce cobra, the service container, and a new, propsed ADR.

---
+++ internal/app/container.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"fmt"
+)
+
+var ErrCannotCreateContainer = fmt.Errorf("cannot create container")
+
+type ContainerFactoryFunc func() (*Container, error)
+
+func NewDefaultParameters() Parameters {
+	return Parameters{}
+}
+
+type Parameters struct {
+	Versions Versions
+}
+
+type services struct {
+}
+
+func NewContainer(versions Versions) *Container {
+	ctr := Container{
+		Parameters: NewDefaultParameters(),
+	}
+
+	ctr.Versions = versions
+
+	return &ctr
+}
+
+type Container struct {
+	Parameters
+	services
+}
